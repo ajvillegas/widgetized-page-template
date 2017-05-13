5b2695ee45664eda73876b6c20e27b06e3b165cb
Alexis J. Villegas <alexis@ajvillegas.com>
2017-05-13 00:56:37 -0700

    Fix typo

--- includes/class-widgetized-page-template.php
+++ includes/class-widgetized-page-template.php
@@ -165,7 +165,7 @@ class Widgetized_Page_Template {
 			
 		}
 
-		// Add a filter to the save post in order to inject out template into the page cache
+		// Add a filter to the save post in order to inject our template into the page cache
 		$this->loader->add_filter( 'wp_insert_post_data', $plugin_templater, 'register_plugin_templates' );
 
 		// Add a filter to the template include in order to determine if the page has our template assigned and return it's path
